83b964b4f4bb51b22f18a8f01bbcd7a68b8134ca
ALEX ROSENFELD <arosenfeld2003@mac.com>
2023-10-28 15:17:32 -0700

    fix typescript error top level await not allowed

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,9 @@ import { duckDBManager } from "./db/duckdb";
 const app = express();
 const DATABASE_PATH = "src/db/duckdb.ts"
 
-const db = async() => {
-    // wrapped in async so we can await the return of initDatabase
-    await duckDBManager.initDatabase(DATABASE_PATH);
-    duckDBManager.getDatabase();
-}
+await duckDBManager.initDatabase(DATABASE_PATH);
+duckDBManager.getDatabase();
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
