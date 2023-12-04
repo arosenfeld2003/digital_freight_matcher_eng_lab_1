6101fff35a38ebef841ec1c3e8fcd396d935e7a1
Zachary Kubli <Kubli.Zachary@gmail.com>
2023-12-03 23:53:44 -0500

    outlining filters mk1

---
+++ src/entry_points/CheckWeight.ts
@@ -0,0 +1,11 @@
+
+
+// definition of an entry point
+// {routeId (from database): { locationsAP: stop[], locationsAD: stop[]} stops as defined in db/types.ts
+
+function check_weight(entryPoint: any, request: Request): boolean {
+    // get list of weights by stop from route
+    
+
+
+}
--- src/entry_points/GetEntryPoints.ts
+++ src/entry_points/GetEntryPoints.ts
@@ -1,22 +1,29 @@
 import DB from '../db/db'
 import { checkProximity } from '../db/helpers';
-import { Request } from '../db/types'; // Add missing import for Request type
+import { Request } from '../db/types';
+import { check_weight } from './CheckWeight';
 
 // Definition of the entry point
 // { routeId: { locationsAP: stop[], locationsAD: stop[]} }
 
-export async function getEntryPoints(request: Request): Promise<Array<{}>> // Add async keyword and export function
-{
-    // Instantiate the database
+export async function getEntryPoints(request: Request): Promise<Array<{}>> {
     const db = DB.getInstance();
-    // Get all routes and stops that are proximal to the request
     let entryPoints: Array<{}> = await checkProximity(request);
 
-    entryPoints = entryPoints.filter(item => 
-      check_weight(item, request) &&
-      check_volume(item, request) &&
-      check_distance(item, request) &&
-      check_time(item, request)      
-    );
+    // Use Promise.all to wait for all checks to complete for each item
+    entryPoints = await Promise.all(entryPoints.map(async item => {
+        const checks = [
+            await check_weight(item, request),
+            await check_volume(item, request),
+            await check_distance(item, request),
+            await check_time(item, request)
+        ];
+        // If all checks pass, return the item. Otherwise, return null.
+        return checks.every(check => check) ? item : null;
+    }));
+
+    // Remove null items from the array
+    entryPoints = entryPoints.filter(item => item !== null);
+
     return entryPoints;
 }
